6177937dbf92c979978e9c6e8f4920775a85ebd4
A. Q. Vassopoli <imagingrelieving633@account.vassopoli.com>
2024-02-04 13:50:47 -0300

    refactor: add prefix for fiap's username and password variable

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -4,8 +4,8 @@ const authFile = 'playwright/.auth/session.json';
 
 setup('authenticate', async ({ page }) => {
   await page.goto(process.env.FIAP_URL??"");
-  await page.getByLabel('RM').fill(process.env.USERNAME??"");
-  await page.getByLabel('Senha').fill(process.env.PASSWORD??"");
+  await page.getByLabel('RM').fill(process.env.FIAP_USERNAME??"");
+  await page.getByLabel('Senha').fill(process.env.FIAP_PASSWORD??"");
   await page.getByRole('button', { name: 'Conectar' }).click();
 
   await page.waitForURL(process.env.FIAP_URL + '/Aluno/Home');
